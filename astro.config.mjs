33431457b073cd44d04cf6b0d32cf35f10350b1a
klhrd <ryderklh@gmail.com>
2026-08-19 18:07:11 +0800

    feat: add server configuration with host and port settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  server: {
+    host: true,
+    port: 4321,
+  },
+
   // 必須設定 site 與 site 底下的子路徑 base
   site: 'https://klhrd.github.io',
   base: '/TickTock', 
